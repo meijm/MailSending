da4aaaefd518ddc29e9fc11d3cd924d2f9508d00
meijm <meijm@outlook.com>
2018-10-07 11:58:59 +0800

    give up

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
-import read_contacts
+from readmail import get_contacts, read_template
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import socket
+
+#socket.setdefaulttimeout(10)
+MY_ADDRESS = 'si131_02_2018@163.com'
+PASSWORD = 'king1230.'
+
+s= smtplib.SMTP(host='smtp.163.com', port=994)#change your prot !!!!!!
+s.ehlo()
+s.starttls()
+s.login(MY_ADDRESS,PASSWORD)
+
+names, emails = get_contacts('mycontacts.txt')
+message_template = read_template('message.txt')
+
+for name, email in zip(names, emails):
+    msg = MIMEMultipart()       # create a message
+
+    # add in the actual person name to the message template
+    message = message_template.substitute(PERSON_NAME=name.title())
+
+    # setup the parameters of the message
+    msg['From']=MY_ADDRESS
+    msg['To']=email
+    msg['Subject']="This is TEST"
+
+    # add in the message body
+    msg.attach(MIMEText(message, 'plain'))
+
+    # send the message via the server set up earlier.
+    s.send_message(msg)
+    
+    del msg
+s.quit()
--- readmail.py
+++ readmail.py
@@ -1,3 +1,4 @@
+from string import Template
 # Function to read the contacts from a given contact file and return a
 # list of names and email addresses
 def get_contacts(filename):
@@ -8,3 +9,8 @@ def get_contacts(filename):
             names.append(a_contact.split()[0])
             emails.append(a_contact.split()[1])
     return names, emails
+
+def read_template(filename):
+    with open(filename,'r',encoding='utf-8') as tempate_file:
+        tempate_file_content = tempate_file.read()
+        return Template(tempate_file_content)
