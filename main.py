a0d677b3358dff91ba2ce61c63d678eca1f73f40
meijm <meijm@outlook.com>
2018-10-07 09:47:27 +0800

    tutorialpoint failed,and switch to FreeCodeCamp

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 import smtplib
+
+sender = 'meijm@outlook.com'
+receivers = ['meijiaming0@163.com']
+
+message = """ Hello meijm!
+Cheers~
+meijm 13262580357
+"""
+
+try:
+    smtpObj=smtplib.SMTP('localhost')
+    smtpObj.sendmail(sender, receivers, message)
+    print("Successfully sent email")
+except SMTPException:
+    print("Error: unable to send email")
+
