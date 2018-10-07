ac9c39f78c607b48b9771a3d42a5ef4c54af416d
meijm <meijm@outlook.com>
2018-10-07 10:11:53 +0800

    add read_contacts.py

---
+++ read_contacts.py
@@ -0,0 +1,10 @@
+# Function to read the contacts from a given contact file and return a
+# list of names and email addresses
+def get_contacts(filename):
+    names = []
+    emails = []
+    with open(filename, mode='r', encoding='utf-8') as contacts_file:
+        for a_contact in contacts_file:
+            names.append(a_contact.split()[0])
+            emails.append(a_contact.split()[1])
+    return names, emails
